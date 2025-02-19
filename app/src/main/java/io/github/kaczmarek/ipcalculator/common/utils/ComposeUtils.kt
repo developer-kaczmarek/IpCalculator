002f5546c56488d087b5c437133ad5b644860938
Lina Podbolotova <developer.kaczmarek@gmail.com>
2025-02-19 21:47:48 +0500

    refactor: add empty state for CalculatorScreen

--- app/src/main/java/io/github/kaczmarek/ipcalculator/common/utils/ComposeUtils.kt
+++ app/src/main/java/io/github/kaczmarek/ipcalculator/common/utils/ComposeUtils.kt
@@ -1,8 +1,13 @@
 package io.github.kaczmarek.ipcalculator.common.utils
 
+import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 
 @Composable
 fun Dp.toPx() = with(LocalDensity.current) { this@toPx.toPx() }
+
+@Composable
+fun ColorScheme.isLight() = this.background.luminance() > 0.5
